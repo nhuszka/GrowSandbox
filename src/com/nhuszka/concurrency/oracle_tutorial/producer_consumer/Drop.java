a02fb74bc6ea11f6a6ffefe89e67c3c6edb44e9a
Nandi <huszka.nandor@gmail.com>
2016-01-31 19:26:24 +0100

    Producer consumer++

--- src/com/nhuszka/concurrency/oracle_tutorial/producer_consumer/Drop.java
+++ src/com/nhuszka/concurrency/oracle_tutorial/producer_consumer/Drop.java
@@ -1,37 +1,33 @@
 package com.nhuszka.concurrency.oracle_tutorial.producer_consumer;
 
 public class Drop {
-	// Message sent from producer
-	// to consumer.
+	// Message sent from producer to consumer
 	private String message;
-	// True if consumer should wait
-	// for producer to send message,
-	// false if producer should wait for
-	// consumer to retrieve message.
+	// True: if consumer should wait for producer to send message
+	// False: if producer should wait for consumer to retrieve message
 	private boolean empty = true;
 
 	public synchronized String take() {
-		// Wait until message is
-		// available.
+		// Wait until message is available.
 		while (empty) {
 			try {
+				System.out.println("TAKE wait");
 				wait();
 			} catch (InterruptedException e) {
 			}
 		}
 		// Toggle status.
 		empty = true;
-		// Notify producer that
-		// status has changed.
+		// Notify producer that status has changed.
 		notifyAll();
 		return message;
 	}
 
 	public synchronized void put(String message) {
-		// Wait until message has
-		// been retrieved.
+		// Wait until message has been retrieved.
 		while (!empty) {
 			try {
+				System.out.println("PUT wait");
 				wait();
 			} catch (InterruptedException e) {
 			}
@@ -40,8 +36,7 @@ public class Drop {
 		empty = false;
 		// Store message.
 		this.message = message;
-		// Notify consumer that status
-		// has changed.
+		// Notify consumer that status has changed.
 		notifyAll();
 	}
 }
